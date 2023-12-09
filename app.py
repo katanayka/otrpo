1faf8b7ce7f933f59b59b2a3279e692a986c6bc9
Katanayka <katanaevdmitry45@gmail.com>
2023-12-09 06:25:52 +0500

    6 hours

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import datetime
 import json
 import requests
-from flask import Flask, request, render_template, redirect, url_for, session, jsonify
+from flask import Flask, request, render_template, redirect, url_for, session, jsonify, flash
 import sqlite3
 import random
 import os
@@ -10,18 +10,330 @@ import ssl
 from email.message import EmailMessage
 from ftplib import FTP
 from io import BytesIO
-from dotenv import load_dotenv
+from dotenv import load_dotenv, dotenv_values
 import redis
+import secrets
+import string
+import dotenv
 
 app = Flask(__name__)
 CACHE_TIMEOUT = 600
 PER_PAGE = 15
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
-DOMAIN = os.getenv("DOMAIN")
-
+found_dotenv = dotenv.find_dotenv('.env')
+print(found_dotenv)
+app = Flask(__name__)
+print(os.getenv("sender_password"))
+app.secret_key = secrets.token_hex(16)
 
 redis_client = redis.from_url(os.getenv("redis"))
+
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL
+    )
+''')
+conn.commit()
+conn.close()
+
+class User(UserMixin):
+    def __init__(self, user_id, username, email, password):
+        self.id = user_id
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def get_id(self):
+        return self.id
+
+    def get_username(self):
+        return self.username
+
+    def get_email(self):
+        return self.email
+
+    def get_password(self):
+        return self.password
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE id = ?', (user_id,))
+    user_data = cursor.fetchone()
+    conn.close()
+    
+    if user_data:
+        return User(user_data[0], user_data[1], user_data[2], user_data[3])
+    return None
+
+# Registration route
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        hashed_password = generate_hash(password)
+
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+
+        cursor.execute('SELECT * FROM users WHERE username = ? OR email = ?', (username, email))
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            conn.close()
+            flash('Username or email already exists', 'error')
+            return redirect(url_for('register'))
+
+        cursor.execute('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', (username, email, hashed_password))
+        conn.commit()
+        conn.close()
+
+        flash('Registration successful', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+    
+verification_codes = {}
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+
+        cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+        user_data = cursor.fetchone()
+
+        conn.close()
+
+        if user_data and check_password(user_data[3], password):
+            verification_code = ''.join(random.choices(string.digits, k=6))
+            verification_codes[username] = verification_code
+            send_verification_code(user_data[2], verification_code)
+            return render_template('enter_verification_code.html', username=username)
+    return render_template('login.html')
+
+def send_verification_code(email, code):
+    sender_email = os.getenv("sender_email")
+    sender_password = os.getenv("sender_password")
+    subject = "Code to login otrpo"
+    body = code
+
+    em = EmailMessage()
+    em["From"] = sender_email
+    em["To"] = email
+    em["Subject"] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(sender_email, sender_password)
+        smtp.sendmail(sender_email, email, em.as_string())
+
+@app.route("/verify_code", methods=['POST'])
+def verify_code():
+    entered_code = request.form['code']
+    username = request.form['username']
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+    user_data = cursor.fetchone()
+
+    conn.close()
+    stored_code = verification_codes.get(username)
+
+    if stored_code and entered_code == stored_code:
+        del verification_codes[username]  # Remove the used verification code
+        user = User(user_data[0], user_data[1], user_data[2], user_data[3])  # Replace with actual user details
+        login_user(user)
+        flash('Login successful', 'success')
+        return redirect(url_for('main'))
+    else:
+        flash('Invalid verification code', 'error')
+        return redirect(url_for('login'))
+
+
+from flask_oauthlib.client import OAuth
+app.config['GITHUB_CLIENT_ID'] = os.getenv("GITHUB_CLIENT_ID")
+app.config['GITHUB_CLIENT_SECRET'] =  os.getenv("GITHUB_CLIENT_SECRET")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
+oauth = OAuth(app)
+
+github = oauth.remote_app(
+    'github',
+    consumer_key=app.config['GITHUB_CLIENT_ID'],
+    consumer_secret=app.config['GITHUB_CLIENT_SECRET'],
+    request_token_params=None,
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize',
+)
+
+@github.tokengetter
+def get_github_oauth_token():
+    return session.get('github_token')
+
+@app.route('/github/login')
+def github_login():
+    return github.authorize(callback=url_for('github_authorized', _external=True))
+
+
+@app.route('/github/authorized')
+@github.authorized_handler
+def github_authorized(resp):
+    if resp is None or 'access_token' not in resp:
+        flash('Access denied: reason={} error={}'.format(
+            request.args['error_reason'],
+            request.args['error_description']
+        ), 'error')
+        return redirect(url_for('main'))
+
+    session['github_token'] = (resp['access_token'], '')
+    user_info = github.get('user')
+    username = user_info.data['login']
+
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+    user_data = cursor.fetchone()
+
+    if user_data:
+        user = User(user_data[0], user_data[1], user_data[2], user_data[3])
+        login_user(user)
+        flash('Login via GitHub successful', 'success')
+        conn.close()
+        return redirect(url_for('main'))
+    else:
+        cursor.execute('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', (username, username, generate_hash(username)))
+        conn.commit()
+        cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+        user_data = cursor.fetchone()
+        conn.close()
+        print(user_data)
+        user = User(user_data[0], user_data[1], user_data[2], user_data[3])
+        login_user(user)
+        flash('Login via GitHub successful', 'success')
+        return redirect(url_for('main'))
+
+recovery_codes = {}
+
+@app.route("/recover_password", methods=['GET', 'POST'])
+def recover_password():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users WHERE username = ? AND email = ?', (username, email))
+        user_data = cursor.fetchone()
+
+        conn.close()
+
+        if user_data:
+            recovery_code = ''.join(random.choices(string.digits, k=6))
+            recovery_codes[username] = {"code": recovery_code, "email": email}
+            send_recovery_code(email, recovery_code)
+            return render_template('recover_password.html', username=username)
+
+    return render_template('recover_password_request.html')
+
+def send_recovery_code(email, code):
+    sender_email = os.getenv("sender_email")
+    sender_password = os.getenv("sender_password")
+    subject = "Code to login otrpo"
+    body = code
+
+    em = EmailMessage()
+    em["From"] = sender_email
+    em["To"] = email
+    em["Subject"] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(sender_email, sender_password)
+        smtp.sendmail(sender_email, email, em.as_string())
+
+@app.route("/recover_password/submit", methods=['POST'])
+def recover_password_submit():
+    username = request.form['username']
+    entered_code = request.form['code']
+    new_password = request.form['new_password']
+    stored_recovery_data = recovery_codes.get(username)
+
+    if stored_recovery_data and entered_code == stored_recovery_data['code']:
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('UPDATE users SET password = ? WHERE username = ?', (generate_hash(new_password), username))
+        conn.commit()
+        conn.close()
+
+        # Remove the used recovery code
+        del recovery_codes[username]
+
+        flash('Password recovery successful', 'success')
+        return redirect(url_for('login'))
+    else:
+        flash('Invalid recovery code', 'error')
+
+@app.route('/github/logout')
+@login_required
+def github_logout():
+    logout_user()
+    flash('Logout successful', 'success')
+    return redirect(url_for('main'))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('Logout successful', 'success')
+    return redirect(url_for('main'))
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+def generate_hash(password):
+    return generate_password_hash(password, method='pbkdf2:sha256')
+
+def check_password(hashed_password, password):
+    return check_password_hash(hashed_password, password)
+
+@app.route("/profile")
+@login_required
+def profile():
+    return render_template('profile.html', user=current_user)
+
+def update_user_password(user_id, new_password):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    hashed_password = hash_password(new_password)
+    cursor.execute('UPDATE users SET password = ? WHERE id = ?', (hashed_password, user_id))
+
+    conn.commit()
+    conn.close()
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
@@ -245,12 +557,15 @@ conn.commit()
 conn.close()
 
 def record_battle(winner_id, player_id, enemy_id, rounds):
+    if current_user.is_authenticated: 
+        a = current_user.get_username()
+    else:
+        a = None
     timestamp = datetime.datetime.now()
-    
     conn = sqlite3.connect('battles.db')
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO battles (timestamp, player_id, enemy_id, winner_id, rounds) VALUES (?, ?, ?, ?, ?)',
-                   (timestamp, player_id, enemy_id, winner_id, rounds))
+    cursor.execute('INSERT INTO battles (timestamp, player_id, enemy_id, winner_id, rounds, user) VALUES (?, ?, ?, ?, ?, ?)',
+                   (timestamp, player_id, enemy_id, winner_id, rounds, a))
     
     conn.commit()
     conn.close()
@@ -324,6 +639,21 @@ c.execute('''CREATE TABLE IF NOT EXISTS ratings (
 
 conn.commit()
 conn.close()
+
+conn = sqlite3.connect('users.db')
+c = conn.cursor()
+c.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username TEXT UNIQUE,
+        email TEXT UNIQUE,
+        password TEXT
+    )
+''')
+
+conn.commit()
+conn.close()
+
 
 @app.route("/POCHTA", methods=["POST"])
 def send_fast_fight_result():
@@ -415,7 +745,6 @@ def create_Markdown(data):
     markdown_text += f"![Image]({data['front_default']})\n"
     markdown_text += f"**Types:** {', '.join(data['types'])}\n"
     return markdown_text
-
 
 if __name__ == '__main__':
     app.run()
